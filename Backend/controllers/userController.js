1ca0e7037093b910279a3ff74d63a6517f24a977
Cyber_Infernox <sayonkar@gmail.com>
2022-12-25 20:14:52 +0530

    Authorization
    Adding signup and login page

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require("../models/userModel");
+
+// Login user
+const loginUser = async (req, res) => {
+  res.json({ mssg: "Login user" });
+};
+
+// Signup user
+const signupUser = async (req, res) => {
+  res.json({ mssg: "Signup user" });
+};
+
+module.exports = { signupUser, loginUser };
